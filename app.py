81c1ba5c070bf6d225bca7260395b3c3cffa6149
eliftuc <eliftuc74@gmail.com>
2025-03-16 01:42:10 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:955d993cd08ceb139df3622342a63ddd0236281d26fb874c63011f942b282e81
+size 6852
---
+++ static/js/scripts.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3133e7fd014a2767b703a3f2aaea2920b54438dcd20d91358a8068e8b10724a
+size 1645
